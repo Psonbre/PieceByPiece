f4db88f1cae805e843cb8513ad483828188ac83e
Psonbre <adam.maps.2003@gmail.com>
2024-12-10 14:06:20 -0500

    added fullscreen with f11

--- Scripts/Manager/SceneManager.gd
+++ Scripts/Manager/SceneManager.gd
@@ -151,4 +151,11 @@ func updated_discord_presence(main : String, state := ""):
 		discord_rpc.details = main
 		discord_rpc.state = state
 		discord_rpc.refresh()
-	
+
+func  _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Fullscreen") :
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN :
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		else :
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+			
