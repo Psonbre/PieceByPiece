b7e1e4f4283e2724850b4e484a5ed7d32760612c
Philippe St-Laurent-Recoura <71201023+Philou213@users.noreply.github.com>
2024-12-13 16:51:04 -0500

    moving back button

--- Scripts/Buttons/BackScript.gd
+++ Scripts/Buttons/BackScript.gd
@@ -1,7 +1,14 @@
-extends Button
+extends FloatingUI
 
 @export var scroll_direction := Vector2(1,0)
 
 func _on_pressed() -> void:
 	SubSystemManager.get_sound_manager().play_sound(preload("res://Assets/Sounds/button_click.ogg"), -8, 1)
 	SubSystemManager.get_scene_manager().load_main_menu(scroll_direction)
+
+func _on_mouse_entered():
+	scale = 1.1 * Vector2.ONE
+
+
+func _on_mouse_exited():
+	scale = 1.0 * Vector2.ONE
