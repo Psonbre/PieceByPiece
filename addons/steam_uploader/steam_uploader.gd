077df71bc6e444362b1441cf3df592d8eaf413dc
Psonbre <adam.maps.2003@gmail.com>
2025-01-23 00:03:43 -0500

    htto requests done

--- addons/steam_uploader/steam_uploader.gd
+++ addons/steam_uploader/steam_uploader.gd
@@ -2,28 +2,69 @@
 extends EditorPlugin
 
 var dock := preload("res://addons/steam_uploader/dock.tscn").instantiate()
+const SETTINGS_PATH_PREFIX = "SteamUploader/presets/"
 
 func _enter_tree() -> void:
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_BL, dock)
-	dock.button.connect("pressed", func() : build_project())
+	dock.get_node("Build/Build").connect("pressed", func(): build_project())
+	load_inputs()
 
 func _exit_tree() -> void:
 	remove_control_from_docks(dock)
+	save_inputs()
 
+func load_inputs():
+	var settings = EditorInterface.get_editor_settings()
+	for child in dock.find_children("*"):
+		if child is LineEdit: 
+			var setting_path = SETTINGS_PATH_PREFIX + child.name
+			if settings.has_setting(setting_path):
+				child.text = settings.get_setting(setting_path)
+		if child is SpinBox: 
+			var setting_path = SETTINGS_PATH_PREFIX + child.name
+			if settings.has_setting(setting_path):
+				child.value = settings.get_setting(setting_path)
+		elif child is OptionButton: 
+			var setting_path = SETTINGS_PATH_PREFIX + child.name
+			if settings.has_setting(setting_path):
+				child.selected = settings.get_setting(setting_path)
+	dock._on_build_path_changed("")
+	
+func save_inputs():
+	var settings = EditorInterface.get_editor_settings()
+	for child in dock.find_children("*"):
+		var setting_path = SETTINGS_PATH_PREFIX + child.name
+		if child is LineEdit: 
+			settings.set_setting(setting_path, child.text)
+			if child.secret :
+				settings.add_property_info({
+					"name": setting_path,
+					"type": "string",
+					"usage" : PROPERTY_USAGE_NO_EDITOR,
+				})
+		if child is SpinBox: 
+			settings.set_setting(setting_path, child.value)
+		elif child is OptionButton:
+			settings.set_setting(setting_path, child.selected)
+	
 func build_project():
+	# Validate inputs dynamically
+	for child in dock.find_children("*"):
+		if child is LineEdit and child.text.is_empty():
+			push_error(child.name + " must not be empty")
+			return
 	# Get the path to the current Godot executable
 	var godot_executable = OS.get_executable_path()
 
 	# Construct the command line arguments
 	var args = [
-		godot_executable,
-		"--no-window",
-		"--export-release", dock.build_preset, dock.build_path
+		"--headless",
+		"--export-release",
+		dock.get_value("Build Preset"), 
+		str(dock.get_value("Build Path")) + "\\" + str(dock.get_value("File Name")),
+		"--quit"
 	]
 
 	var output = []
 	var result = OS.execute(godot_executable, args, output)
-	if result == OK:
-		print("Build process started successfully.")
-	else:
-		print(output)
+	dock.get_node("ConfirmationDialog").popup_centered()
