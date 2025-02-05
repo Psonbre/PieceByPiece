87a0fe0091dc256784e90cd1b5248e16bcf272b1
Philippe St-Laurent-Recoura <71201023+Philou213@users.noreply.github.com>
2025-02-05 14:08:14 -0500

    Translate level label

--- Scripts/level_title.gd
+++ Scripts/level_title.gd
@@ -1,11 +1,24 @@
 extends FloatingUI
 
+
 func _ready() -> void:
 	super._ready()
 	modulate.a = 0
 	
 func show_title():
+	translate()
 	return create_tween().tween_property(self, "modulate:a", 1, 0.3)
 	
 func hide_title():
 	return create_tween().tween_property(self, "modulate:a", 0, 0.3)
+
+func translate():
+	var level_text = self.text
+		
+	var regex = RegEx.new()
+	regex.compile(r"\d+")
+	
+	var result = regex.search(level_text)
+	if result:
+		var level_number = int(result.get_string()) # Convert to int
+		self.text = tr("LEVEL %d") % level_number
