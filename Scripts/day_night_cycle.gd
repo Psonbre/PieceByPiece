b102f9d1202ad57edd49c2bcd03b07dc9faa40bc
Psonbre <adam.maps.2003@gmail.com>
2025-02-06 19:42:13 -0500

    changed day night gradiant

--- Scripts/day_night_cycle.gd
+++ Scripts/day_night_cycle.gd
@@ -1,15 +1,17 @@
 extends Polygon2D
 
-@onready var god_rays: Polygon2D = $"../GodRays"
 
+const day_lenght := 60 * 5
+
+@onready var god_rays: Polygon2D = $"../GodRays"
 @export var gradient : GradientTexture1D
+@export var god_rays_gradient : GradientTexture1D
 @export var time := 0.0 :
 	set(value) :
 		var delta := value - time
-		god_rays.polygon[3].x -= delta * 2560.0 * 2.0
-		god_rays.polygon[2].x -= delta * 2560.0 * 2.0
-		god_rays.polygon[7].x -= delta * 2560.0 * 2.0
-		god_rays.polygon[4].x -= delta * 2560.0 * 2.0
 		time = value
-		god_rays.modulate.a = pingpong(-time, 1)
 		color = gradient.gradient.sample(pingpong(time, 1))
+		god_rays.color = god_rays_gradient.gradient.sample(pingpong(time, 1))
+
+func _process(delta: float) -> void:
+	time += delta / day_lenght
