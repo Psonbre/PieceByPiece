fcc31284be249dc63b873bb3471ac8b1cc4d2b4b
Philippe St-Laurent-Recoura <71201023+Philou213@users.noreply.github.com>
2025-02-11 22:49:16 -0500

    Add shorter and fade in to intro

--- Scripts/intro.gd
+++ Scripts/intro.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+@onready var player = $VideoStreamPlayer
+@export var duration : float = 1
+
+func _ready() -> void:
+	var tween := create_tween()
+	tween.tween_property(player, "modulate:a", 1, duration).set_ease(Tween.EASE_OUT)
+
+
 func _process(_delta):
 	if Input.is_action_just_pressed("skip_intro"):
 		SubSystemManager.get_music_manager().play_music(preload("res://Assets/Music/Music.mp3"))
