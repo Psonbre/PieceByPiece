bc6c70774541f3add0a353c0e8ecc11f241741ad
Psonbre <adam.maps.2003@gmail.com>
2025-02-12 13:23:25 -0500

    fixed reset level while pause animation is playing

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -125,8 +125,9 @@ func _on_level_menu_on_quit_clicked():
 
 func _on_level_menu_on_restart_clicked():
 	var scene_manager = SubSystemManager.get_scene_manager()
-	if can_reset and scene_manager.old_screen != self and reset_level_cooldown.is_stopped() and !Player.winning:
+	if can_reset and scene_manager.old_screen != self and reset_level_cooldown.is_stopped() and !Player.winning and (!pause_menu.current_tween or !pause_menu.current_tween.is_running()):
 		if scene_manager.reset_scene() : 
+			pause_menu.blur.visible = false
 			can_reset = false
 
 
